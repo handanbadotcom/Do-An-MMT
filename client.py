e920b20cede687b17db45f8d37496c3e6f4af705
handanbadotcom <20120149@student.hcmus.edu.vn>
2021-12-04 17:57:48 +0700

    tuyệt vọng
    lỗi cđg nhưng ko biết sửa

--- client.py
+++ client.py
@@ -7,4 +7,17 @@ SERVER = '127.0.0.1' #thay đổi địa chỉ theo địa chỉ của server
 ADDRESS = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDRESS)
+client.connect(ADDRESS) 
+
+def send_msg(msg):
+    message= msg.encode(FORMAT)
+    msg_length= len(message)
+    send_length= str(msg_length).encode(FORMAT)
+    send_length=  b' '*(HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send_msg("alo")
+input()
+send_msg("break")    
+
--- server.py
+++ server.py
@@ -2,27 +2,28 @@ import socket
 import threading
 
 FORMAT = 'utf-8'
-HEADER = 64
+HEADER = 64 #chiều dài gói tin
 PORT = 5080
 SERVER = '127.0.0.1' #socket.gethostbyname(socket.gethostname())
 ADDRESS=(SERVER,PORT)
 DISSMSG = "break"
-
+#khởi tạo socket
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDRESS)
 
 def handle_client(conn, addr):
     print("[NEW CONNECTION]", {addr})
     connection = True
-    while connection:
-        message_length= conn.recv(HEADER).decode(FORMAT)
-        message_length = int(message_length)
-        message = conn.recv(message_length).decode(FORMAT)
-        if message == DISSMSG: 
-            connection = False
-        print(addr,':',message)
-
+    while connection!=None:
+        msg_length= conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISSMSG: 
+                connection = False
+            print(addr,':',msg)    
     conn.close()
+
 def start():
     server.listen()
     print("[LISTEN]", SERVER)
@@ -30,7 +31,7 @@ def start():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print("[ACTIVE CONNECTION]", {threading.activeCount()})
+        print("[ACTIVE CONNECTION]", {threading.active_count()})
 
 
 print("[STARTING]")
