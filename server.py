cf3275773b4cebc519781fe56393ab9138731c18
handanbadotcom <20120149@student.hcmus.edu.vn>
2021-12-04 14:29:11 +0700

    code server

--- server.py
+++ server.py
@@ -1,24 +1,36 @@
 import socket
 import threading
 
-PORT = 5050 
+FORMAT = 'utf-8'
+HEADER = 64
+PORT = 5070
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDRESS=(SERVER,PORT)
 
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDRESS)
 
-def handle_client(connection, address):
-    pass
+def handle_client(conn, addr):
+    print("[NEW CONNECTION]", {addr})
+    connection = True
+    while connection:
+        message_length= conn.recv(HEADER).decode(FORMAT)
+        message_length = int(message_length)
+        message = conn.recv(message_length).decode(FORMAT)
+        if message == "break": 
+            connection = False
+        print(addr,':',message)
 
+    conn.close()
 def start():
     server.listen()
     while True:
-        connection, address = server.accept()
-        thread = threading.Thread(target=handle_client, args=(connection, address))
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print({threading.activeCount()})
+        print("[ACTIVE CONNECTION]", {threading.activeCount()})
 
 
+print("[STARTING]")
 print(SERVER)
 start()
